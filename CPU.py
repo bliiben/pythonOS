cdd3506281320b45896606d7180480a19c591f95
bliiben <bliiben.dev@gmail.com>
2015-08-24 14:11:13 +0100

    CPU and OS working, shared memory problem

---
+++ CPU.py
@@ -0,0 +1,57 @@
+import OSException
+		
+def execute(program, memory=[0 for i in xrange(10)], state=0,numberOfOperation=1):
+	print (program,state,memory)
+	maxOp = state + numberOfOperation
+	while state < len(program) and state < maxOp:
+		#type of operation
+		if(program[state][0]=='r'):
+			print("operation "+str(state)+" is assign")
+			doAssign(program[state],memory)
+		elif(program[state][0]=='j'):
+			print("operation "+str(state)+" is jump")
+		else:
+			raise InvalidOperation("Program is not valid, error at op: "+str(state))
+		
+		state+=1
+
+	return {"program":program,"memory":memory,"state":state}
+
+def doAssign(operation,memory):
+	endRegister,op =operation.split(':')
+	endRegister=int(endRegister[1])
+	print("Operation executed", "register_"+str(endRegister), "get" ,op)
+	# is it an assign or an op ?
+	if('+' in op):
+		print("is a +")
+		av,ap = op.split('+')
+		memory[endRegister]=getValROC(av,memory) + getValROC(ap,memory)
+
+	elif('-' in op):
+		print("is a -")
+		av,ap = op.split('-')
+		memory[endRegister]=getValROC(av,memory) - getValROC(ap,memory)
+	else:
+		memory[endRegister]=getValROC(op,memory)
+
+	print(memory)
+
+def getValROC(string,memory):
+	r=getRegOrC(string)
+	if(r[0]=='r'):
+		return memory[r[1]]
+	else:
+		return r[1]
+
+def getRegOrC(string):
+	return ( ('r' if string[0] == 'r' else 'c'), \
+		int(string[1] if string[0] == 'r' else string) )
+
+def startProgram(programString,numberOfOperation=1):
+	return execute(programString.split(','),numberOfOperation=numberOfOperation)
+
+def main():
+	startProgram("r0:1,r2:4+r0,r1:r2-r0",1)
+
+if __name__ == '__main__':
+	main()
--- os.py
+++ os.py
@@ -1,57 +1,50 @@
 import OSException
-		
+import CPU
+from collections import deque
+import threading 
 
+class OS(threading.Thread):
+	"""docstring for OS"""
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.programs = deque()
+		self.lock = threading.RLock()
+		self.isUpdates = threading.Event()
 
-def execute(program, memory=[0 for i in xrange(10)], state=0):
-	print (program,state,memory)
-	
-	while state < len(program):
-		#type of operation
-		if(program[state][0]=='r'):
-			print("operation "+str(state)+" is assign")
-			doAssign(program[state],memory)
-		elif(program[state][0]=='j'):
-			print("operation "+str(state)+" is jump")
-		else:
-			raise InvalidOperation("Program is not valid, error at op: "+str(state))
-		
-		state+=1
+	def addProgram(self,program,client):
+		with self.lock:
+			self.programs.appendleft({"programString":program,"client":client})
+		self.isUpdates.set()
 
-def doAssign(operation,memory):
-	endRegister,op =operation.split(':')
-	endRegister=int(endRegister[1])
-	print("Operation executed", "register_"+str(endRegister), "get" ,op)
-	# is it an assign or an op ?
-	if('+' in op):
-		print("is a +")
-		av,ap = op.split('+')
-		memory[endRegister]=getValROC(av,memory) + getValROC(ap,memory)
+	def run(self):
+		print("Waiting....")
+		while self.isUpdates.wait():
+			print("Something to process !")
+			programToRun=None
+			with self.lock:
+				if len (self.programs) != 0:
+					print("number of programs",len (self.programs))
+					programToRun = self.programs.pop()
 
-	elif('-' in op):
-		print("is a -")
-		av,ap = op.split('-')
-		memory[endRegister]=getValROC(av,memory) - getValROC(ap,memory)
-	else:
-		memory[endRegister]=getValROC(op,memory)
+			if programToRun != None:
+				#programToRun["client"].running=True
+				#Has the program already been started once ?
+				if "program" not in programToRun.keys():
+					programToRun.update(CPU.startProgram(programToRun["programString"]))
+				else:
+					programToRun.update(CPU.execute(programToRun["program"],programToRun["memory"],programToRun["state"]))
 
-	print(memory)
+				#programToRun["client"].running=False
+				with self.lock:
+					if len(programToRun["program"])==programToRun["state"]:
+						print("program finished", "memory is ",hex(id(programToRun["memory"])), "of" ,programToRun["programString"])
+					else:
+						self.programs.appendleft(programToRun)
+			else :
+				self.isUpdates.clear()
+				print ("Waiting for new programs to process...")
 
-def getValROC(string,memory):
-	r=getRegOrC(string)
-	if(r[0]=='r'):
-		return memory[r[1]]
-	else:
-		return r[1]
-
-def getRegOrC(string):
-	return ( ('r' if string[0] == 'r' else 'c'), \
-		int(string[1] if string[0] == 'r' else string) )
-
-def startProgram(programString):
-	execute(programString.split(','))
-
-def main():
-	startProgram("r0:1,r2:4+r0,r1:r2-r0")
-
-if __name__ == '__main__':
-	main()
+os=OS()
+os.start()
+os.addProgram("r0:1,r2:4+r0,r1:r2-r0",{"running":False})
+os.addProgram("r0:1,r0:r0+r0,r0:r0+r0,r0:r0+r0,r0:r0+r0",{"running":False})
